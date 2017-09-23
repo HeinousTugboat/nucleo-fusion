8bc3c7f736bb9cebe039c972cafd6b279908025c
HeinousTugboat <admin@heinousindustries.com>
2017-09-23 13:29:52 -0400

    fix: trunc pipe accuracy was defaulting to 0, throwing errors

--- src/app/shared/trunc.pipe.ts
+++ src/app/shared/trunc.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncPipe implements PipeTransform {
 
-  transform(value: number, acc: number): any {
+  transform(value: number, acc: number = 3): any {
     return value.toPrecision(acc + 1);
   }
 
