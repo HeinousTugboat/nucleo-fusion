74931412d42c519379cc52052f07eb1cade8ad21
HeinousTugboat <admin@heinousindustries.com>
2017-09-23 13:31:12 -0400

    feat(upgrade-panel.component): initial structure

--- src/app/nucleo/upgrade-panel/upgrade-panel.component.ts
+++ src/app/nucleo/upgrade-panel/upgrade-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { UpgradeType } from '../../models/upgrade.model';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-upgrade-panel',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upgrade-panel.component.css']
 })
 export class UpgradePanelComponent implements OnInit {
+  @Input() upgs: UpgradeType[];
 
   constructor() { }
 
