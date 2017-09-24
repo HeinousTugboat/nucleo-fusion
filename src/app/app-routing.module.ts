2bcbc135a17afd1771164a2d4130b48838836a32
HeinousTugboat <admin@heinousindustries.com>
2017-09-23 20:44:59 -0400

    chore: clean up routing and layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'lab', component: LabSettingsComponent }
 ];
 
